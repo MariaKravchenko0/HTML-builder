e961ac26ed5232125439bd28249641339f86720f
MariaKravchenko0 <kravchenkomaria00@gmail.com>
2022-05-20 18:50:37 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+(async function () {
+  await fs.rm(path.join(__dirname, 'files-copy'), {
+    recursive: true,
+    force: true,
+  });
+  await fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+  const sourceFolder = await fs.readdir(path.join(__dirname, 'files'), {
+    withFileTypes: true,
+  });
+  const files = sourceFolder.filter((file) => file.isFile());
+
+  for (let file of files) {
+    await fs.copyFile(
+      path.join(__dirname, 'files', file.name),
+      path.join(__dirname, 'files-copy', file.name)
+    );
+  }
+})();
