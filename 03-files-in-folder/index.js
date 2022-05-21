7790bd179ba42d11ae4945569d97646b6d4e8e35
MariaKravchenko0 <kravchenkomaria00@gmail.com>
2022-05-21 13:40:54 +0300

    refactor: rewrite function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,28 +2,24 @@ const fs = require('fs/promises');
 const path = require('path');
 
 (async function () {
-  try {
-    const folderContent = await fs.readdir(
-      path.join(__dirname, 'secret-folder'),
-      {
-        withFileTypes: true,
-      }
+  const folderContent = await fs.readdir(
+    path.join(__dirname, 'secret-folder'),
+    {
+      withFileTypes: true,
+    }
+  );
+
+  const files = folderContent.filter((file) => file.isFile());
+
+  for (let file of files) {
+    const fileSize = await fs.stat(
+      path.join(__dirname, 'secret-folder', file.name)
     );
 
-    const files = folderContent.filter((file) => file.isFile());
-
-    for (let file of files) {
-      const fileSize = await fs.stat(
-        path.join(__dirname, 'secret-folder', file.name)
-      );
-
-      console.log(
-        `${file.name} - ${path.extname(file.name).slice(1)} - ${
-          fileSize.size
-        } bytes `
-      );
-    }
-  } catch (err) {
-    console.error(err);
+    console.log(
+      `${file.name} - ${path.extname(file.name).slice(1)} - ${
+        fileSize.size
+      } bytes `
+    );
   }
 })();
